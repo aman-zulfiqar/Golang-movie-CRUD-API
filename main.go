64ce828ffe86ca150e02b9d76c28592f9e881ca7
Amanzulfiqar76 <amanzulfiqar75@gmail.com>
2025-03-29 16:13:49 +0500

    Refactored movie API

--- main.go
+++ main.go
@@ -1,50 +1,51 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var client *mongo.Client
+var movieCollection *mongo.Collection
+
 type Movie struct {
-	ID       string    `json:"id"`
-	Isbn     string    `json:"isbn"`
-	Title    string    `json:"title"`
-	Director *Director `json:"director"`
+	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Isbn     string             `json:"isbn" bson:"isbn"`
+	Title    string             `json:"title" bson:"title"`
+	Director *Director          `json:"director" bson:"director"`
 }
 
 type Director struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
+	Firstname string `json:"firstname" bson:"firstname"`
+	Lastname  string `json:"lastname" bson:"lastname"`
 }
 
-var movies []Movie
-
-// Get all movies
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	cursor, err := movieCollection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		http.Error(w, "Failed to fetch movies", http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	var movies []Movie
+	if err = cursor.All(context.TODO(), &movies); err != nil {
+		http.Error(w, "Error parsing movies", http.StatusInternalServerError)
+		return
+	}
 	json.NewEncoder(w).Encode(movies)
 }
 
-// Get a single movie by ID
-func getMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for _, movie := range movies {
-		if movie.ID == params["id"] {
-			json.NewEncoder(w).Encode(movie)
-			return
-		}
-	}
-	http.Error(w, "Movie not found", http.StatusNotFound)
-}
-
-// Create a new movie
 func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
@@ -52,61 +53,43 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
-	// Generate a random ID and convert it to a string
-	movie.ID = strconv.Itoa(rand.Intn(1000000))
-	movies = append(movies, movie)
+	movie.ID = primitive.NewObjectID()
+	_, err := movieCollection.InsertOne(context.TODO(), movie)
+	if err != nil {
+		http.Error(w, "Failed to create movie", http.StatusInternalServerError)
+		return
+	}
 	json.NewEncoder(w).Encode(movie)
 }
 
-// Update an existing movie by ID
-func updateMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for i, movie := range movies {
-		if movie.ID == params["id"] {
-			var updatedMovie Movie
-			if err := json.NewDecoder(r.Body).Decode(&updatedMovie); err != nil {
-				http.Error(w, "Invalid request payload", http.StatusBadRequest)
-				return
-			}
-			// Keep the existing movie ID
-			updatedMovie.ID = movie.ID
-			movies[i] = updatedMovie
-			json.NewEncoder(w).Encode(updatedMovie)
-			return
-		}
-	}
-	http.Error(w, "Movie not found", http.StatusNotFound)
-}
-
-// Delete a movie by ID
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for i, movie := range movies {
-		if movie.ID == params["id"] {
-			// Remove the movie from the slice
-			movies = append(movies[:i], movies[i+1:]...)
-			json.NewEncoder(w).Encode(movies)
-			return
-		}
+	id, err := primitive.ObjectIDFromHex(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid movie ID", http.StatusBadRequest)
+		return
 	}
-	http.Error(w, "Movie not found", http.StatusNotFound)
+	res, err := movieCollection.DeleteOne(context.TODO(), bson.M{"_id": id})
+	if err != nil || res.DeletedCount == 0 {
+		http.Error(w, "Movie not found", http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(map[string]string{"message": "Movie deleted"})
 }
 
 func main() {
+	ctx := context.TODO()
+	var err error
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
+	}
+	movieCollection = client.Database("moviedb").Collection("movies")
+
 	r := mux.NewRouter()
-
-	// Preload some movie data
-	movies = append(movies,
-		Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{Firstname: "Muhammad", Lastname: "Aman"}},
-		Movie{ID: "2", Isbn: "45455", Title: "Movie Two", Director: &Director{Firstname: "Muhammad", Lastname: "Zain"}},
-	)
-
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Println("Server running on port 8000...")
